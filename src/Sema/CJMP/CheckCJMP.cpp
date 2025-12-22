61d8d52a3aac9e284614e1294a3d0023bb1c924a
Mashkov Sergey <mashkov.sergey@huawei.com>
2025-12-22 16:27:13 +0300

    fix: fix handling common variables
    Signed-off-by: Mashkov Sergey <mashkov.sergey@huawei.com>

--- src/Sema/CJMP/CheckCJMP.cpp
+++ src/Sema/CJMP/CheckCJMP.cpp
@@ -603,8 +603,9 @@ void MPTypeCheckerImpl::CheckReturnAndVariableTypes(AST::Package& pkg)
                 auto& platformVar = *StaticCast<VarDecl>(platform);
 
                 CheckMatchedVariableTypes(platformVar, commonVar);
-            } else if (!common->TestAttr(Attribute::INITIALIZED) && common->TestAttr(Attribute::IMPORTED)) {
-                // this has been postponed
+            } else if (!common->TestAttr(Attribute::INITIALIZED) && common->TestAttr(Attribute::FROM_COMMON_PART)) {
+                // this has been postponed, we need this to handle the case when a common var has no matched specified
+                // but it does't fail because there is an initializer for it
                 DiagNotMatchedPlatformDecl(diag, *common);
             }
             return VisitAction::SKIP_CHILDREN;
@@ -746,11 +747,38 @@ bool MPTypeCheckerImpl::MatchCJMPDeclAnnotations(
     const std::vector<AST::AnnotationKind>& annotations, const AST::Decl& common, const AST::Decl& platform) const
 {
     for (auto anno : annotations) {
-        if (common.HasAnno(anno) != platform.HasAnno(anno)) {
-            // Keep silent due to overloaded common funcs.
-            if (common.astKind != ASTKind::FUNC_DECL) {
-                diag.DiagnoseRefactor(
-                    DiagKindRefactor::sema_platform_has_different_annotation, platform, DeclKindToString(platform));
+        bool commonHas{common.HasAnno(anno)};
+        bool platformHas{platform.HasAnno(anno)};
+
+        if (commonHas != platformHas) {
+            if (anno == AnnotationKind::DEPRECATED) {
+                if (commonHas) {
+                    platform.annotations.emplace_back(ASTCloner::Clone(GetDeprecated(common)));
+                } else {
+                    diag.DiagnoseRefactor(DiagKindRefactor::sema_platform_has_deprecated_annotation,
+                            *GetDeprecated(platform), "Deprecated", DeclKindToString(platform),
+                            platform.identifier.Val())
+                        .AddNote("platform declarations implicitly inherit deprecation from the common declarations");
+                    return false;
+                }
+            } else {
+                // Keep silent due to overloaded common funcs.
+                // this shouldn't work like this and the rest of the code in this function proves it
+                // instead, we should match functions first by name and parameter types
+                // and after that we should post-check the annotations for already matched pairs
+                if (common.astKind != ASTKind::FUNC_DECL) {
+                    diag.DiagnoseRefactor(
+                        DiagKindRefactor::sema_platform_has_different_annotation, platform, DeclKindToString(platform));
+                }
+                return false;
+            }
+        } else {
+            if (platformHas && anno == AnnotationKind::DEPRECATED) {
+                diag.DiagnoseRefactor(DiagKindRefactor::sema_platform_has_deprecated_annotation,
+                        *GetDeprecated(platform), "Deprecated", DeclKindToString(platform),
+                        platform.identifier.Val())
+                    .AddNote("platform declarations implicitly inherit deprecation from the common declarations");
+                return false;
             }
             return false;
         }
