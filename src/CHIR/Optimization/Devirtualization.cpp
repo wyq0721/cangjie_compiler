f9ecc09526410ac039beb806b10820210f997c1f
Tadddy <yinkejie@huawei.com>
2026-01-22 22:22:17 +0800

    fix: block helper inst error in chir devirtualization
    Signed-off-by: Tadddy <yinkejie@huawei.com>

--- src/CHIR/Optimization/Devirtualization.cpp
+++ src/CHIR/Optimization/Devirtualization.cpp
@@ -323,7 +323,7 @@ void Devirtualization::InstantiateFuncIfPossible(CHIRBuilder& builder, std::vect
 
             auto oriBlockGroup = callee->GetBody();
             BlockGroupCopyHelper helper(builder);
-            helper.GetInstMapFromApply(*apply);
+            helper.GetInstMapFromApply(*apply, newFunc);
             auto [newGroup, newBlockGroupRetValue] = helper.CloneBlockGroup(*oriBlockGroup, *newFunc);
             newFunc->InitBody(*newGroup);
             newFunc->SetReturnValue(*newBlockGroupRetValue);
