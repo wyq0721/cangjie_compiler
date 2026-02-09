0db9975300b75fe8f0a682c3aab13c87e2e50c40
jinbohang <jinbohang1@huawei.com>
2026-02-09 21:47:49 +0800

    fix: fixed the logic error where the input is only obj files and the output type is static
    Signed-off-by: jinbohang <jinbohang1@huawei.com>

--- src/Driver/Toolchains/CJNATIVE/MinGW_CJNATIVE.cpp
+++ src/Driver/Toolchains/CJNATIVE/MinGW_CJNATIVE.cpp
@@ -117,6 +117,11 @@ void MinGW_CJNATIVE::GenerateArchiveTool(const std::vector<TempFileInfo>& objFil
     for (const auto& objFile : objFiles) {
         tool->AppendArg(objFile.filePath);
     }
+    if (objFiles.empty()) {
+        for (const auto& inputObj : driverOptions.inputObjs) {
+            tool->AppendArg(inputObj);
+        }
+    }
     backendCmds.emplace_back(MakeSingleToolBatch({std::move(tool)}));
 }
 
