wyq0721/cangjie_compiler

commit a0c8537853063cc7edcde3315ef2f49014ccb59e

fix: split CheckLegtalityOfReference into functions
Split the function into several to reduce the complexity

Signed-off-by: Mashkov Sergey <mashkov.sergey@huawei.com>

src/Sema/TypeCheckReference.cpp
@@ -731,9 +731,58 @@ void TypeChecker::TypeCheckerImpl::CheckInstanceMemberAccessLegality(
     }
 }
 
+bool TypeChecker::TypeCheckerImpl::CheckLegalityOfReferenceIsSkip(Ptr<Node> node)
+{
+    switch (node->astKind) {
+        case ASTKind::PRIMARY_CTOR_DECL:
+        case ASTKind::ANNOTATION:
+            return true;
+        case ASTKind::FUNC_DECL:
+        case ASTKind::PROP_DECL:
+            // the declaration is deserialized from common part
+            // no need to perform the check as it's already checked during common compilation
+            return node->TestAttr(Attribute::FROM_COMMON_PART);
+        case ASTKind::FUNC_ARG:
+            // If current node is desugared func argument, ignore the checking.
+            return node->TestAttr(Attribute::HAS_INITIAL);
+        default:
+            return false;
+    }
+}
+
+VisitAction TypeChecker::TypeCheckerImpl::CheckLegalityOfReferenceForExpr(
+    unsigned id, ASTContext& ctx, Ptr<AST::Expr> node)
+{
+    if (node->desugarExpr) {
+        // Only check desugared nodes.
+        CheckLegalityOfReference(id, ctx, *node->desugarExpr);
+        return VisitAction::SKIP_CHILDREN;
+    } else if (auto ref = DynamicCast<NameReferenceExpr*>(node); ref && !ref->instTys.empty()) {
+        CheckInstTypeCompleteness(ctx, *ref);
+    } else if (auto re = DynamicCast<RefExpr*>(node)) {
+        if (re->isThis) {
+            CheckUsageOfThis(ctx, *re);
+        } else if (re->isSuper) {
+            CheckUsageOfSuper(ctx, *re);
+        } else {
+            CheckAccessLegalityOfRefExpr(ctx, *re);
+        }
+    } else if (auto ae = DynamicCast<AssignExpr>(node)) {
+        CheckMutationInStruct(ctx, *ae->leftValue);
+    } else if (auto ide = DynamicCast<IncOrDecExpr>(node)) {
+        CheckMutationInStruct(ctx, *ide->expr);
+    }
+    return VisitAction::WALK_CHILDREN;
+}
+
 void TypeChecker::TypeCheckerImpl::CheckLegalityOfReference(ASTContext& ctx, Node& node)
 {
     unsigned id = Walker::GetNextWalkerID();
+    CheckLegalityOfReference(id, ctx, node);
+}
+
+void TypeChecker::TypeCheckerImpl::CheckLegalityOfReference(unsigned id, ASTContext& ctx, Node& node)
+{
     std::function<VisitAction(Ptr<Node>)> postVisit = [this, &ctx](Ptr<Node> node) -> VisitAction {
         if (node->astKind == ASTKind::VAR_DECL) {
             ctx.currentCheckingNodes.pop();
@@ -744,47 +793,17 @@ void TypeChecker::TypeCheckerImpl::CheckLegalityOfReference(ASTContext& ctx, Nod
         }
         return VisitAction::WALK_CHILDREN;
     };
-    std::function<VisitAction(Ptr<Node>)> preVisit = [this, &ctx, &preVisit, &postVisit, id](
-                                                         Ptr<Node> node) -> VisitAction {
-        if (node->astKind == ASTKind::PRIMARY_CTOR_DECL) {
+    std::function<VisitAction(Ptr<Node>)> preVisit = [this, &ctx, id](Ptr<Node> node) -> VisitAction {
+        if (CheckLegalityOfReferenceIsSkip(node)) {
             return VisitAction::SKIP_CHILDREN;
-        } else if (node->astKind == ASTKind::ANNOTATION) {
-            return VisitAction::SKIP_CHILDREN;
-        } else if (node->astKind == ASTKind::VAR_DECL) {
+        }
+        if (node->astKind == ASTKind::VAR_DECL) {
             ctx.currentCheckingNodes.push(node);
         }
-        if (node->IsFuncOrProp() && node->TestAttr(AST::Attribute::FROM_COMMON_PART)) {
-            // the declaration is deserialized from common part
-            // no need to perform the check as it's already checked during common compilation
-            return VisitAction::SKIP_CHILDREN;
-        }
-        // If current node is desugared func argument, ignore the checking.
-        if (node->astKind == ASTKind::FUNC_ARG && node->TestAttr(Attribute::HAS_INITIAL)) {
-            return VisitAction::SKIP_CHILDREN;
-        }
         if (auto expr = DynamicCast<Expr*>(node); expr) {
-            if (expr->desugarExpr) {
-                // Only check desugared nodes.
-                Walker(expr->desugarExpr.get(), id, preVisit, postVisit).Walk();
-                return VisitAction::SKIP_CHILDREN;
-            } else if (auto ref = DynamicCast<NameReferenceExpr*>(expr); ref && !ref->instTys.empty()) {
-                CheckInstTypeCompleteness(ctx, *ref);
-            }
-        }
-        if (auto re = DynamicCast<RefExpr*>(node)) {
-            if (re->isThis) {
-                CheckUsageOfThis(ctx, *re);
-            } else if (re->isSuper) {
-                CheckUsageOfSuper(ctx, *re);
-            } else {
-                CheckAccessLegalityOfRefExpr(ctx, *re);
-            }
+            return CheckLegalityOfReferenceForExpr(id, ctx, expr);
         } else if (auto fa = DynamicCast<FuncArg>(node); fa && fa->withInout) {
             CheckMutationInStruct(ctx, *fa->expr);
-        } else if (auto ae = DynamicCast<AssignExpr>(node)) {
-            CheckMutationInStruct(ctx, *ae->leftValue);
-        } else if (auto ide = DynamicCast<IncOrDecExpr>(node)) {
-            CheckMutationInStruct(ctx, *ide->expr);
         } else if (auto fd = DynamicCast<FuncDecl>(node); fd && IsInstanceConstructor(*fd)) {
             CheckMemberAccessInCtorParamOrCtorArg(ctx, *fd);
         }

src/Sema/TypeCheckerImpl.h
@@ -1597,6 +1597,9 @@ private:
     void CheckTypeArgLegalityOfJArrayCtor(const AST::NameReferenceExpr& re);
     void CheckStaticMemberAccessLegality(const AST::MemberAccess& ma, const AST::Decl& target);
     void CheckInstanceMemberAccessLegality(const ASTContext& ctx, const AST::MemberAccess& ma, const AST::Decl& target);
+    bool CheckLegalityOfReferenceIsSkip(Ptr<AST::Node> node);
+    AST::VisitAction CheckLegalityOfReferenceForExpr(unsigned id, ASTContext& ctx, Ptr<AST::Expr> node);
+    void CheckLegalityOfReference(unsigned id, ASTContext& ctx, AST::Node& node);
     void CheckLegalityOfReference(ASTContext& ctx, AST::Node& node);
     void CheckLegalityOfUnsafeAndInout(AST::Node& root);
 

commit 78406c0b963811e0ba54772d52ac59a050a9f819

fix: legality of generic reference sema
Fix bug in sema introduced in a0c8537853063cc7edcde3315ef2f49014ccb59e
due to wrong refactoring

Signed-off-by: Mashkov Sergey <mashkov.sergey@huawei.com>

src/Sema/TypeCheckReference.cpp
@@ -747,16 +747,13 @@ bool TypeChecker::TypeCheckerImpl::CheckLegalityOfReferenceIsSkip(Ptr<Node> node
     }
 }
 
-VisitAction TypeChecker::TypeCheckerImpl::CheckLegalityOfReferenceForExpr(
-    unsigned id, ASTContext& ctx, Ptr<AST::Expr> node)
+AST::VisitAction TypeChecker::TypeCheckerImpl::CheckLegalityOfReferenceForNameReferenceExpr(
+    ASTContext& ctx, Ptr<AST::NameReferenceExpr> nameRef)
 {
-    if (node->desugarExpr) {
-        // Only check desugared nodes.
-        CheckLegalityOfReference(id, ctx, *node->desugarExpr);
-        return VisitAction::SKIP_CHILDREN;
-    } else if (auto ref = DynamicCast<NameReferenceExpr*>(node); ref && !ref->instTys.empty()) {
-        CheckInstTypeCompleteness(ctx, *ref);
-    } else if (auto re = DynamicCast<RefExpr*>(node)) {
+    if (!nameRef->instTys.empty()) {
+        CheckInstTypeCompleteness(ctx, *nameRef);
+    }
+    if (auto re = DynamicCast<RefExpr*>(nameRef)) {
         if (re->isThis) {
             CheckUsageOfThis(ctx, *re);
         } else if (re->isSuper) {
@@ -764,6 +761,19 @@ VisitAction TypeChecker::TypeCheckerImpl::CheckLegalityOfReferenceForExpr(
         } else {
             CheckAccessLegalityOfRefExpr(ctx, *re);
         }
+    }
+    return VisitAction::WALK_CHILDREN;
+}
+
+VisitAction TypeChecker::TypeCheckerImpl::CheckLegalityOfReferenceForExpr(
+    unsigned id, ASTContext& ctx, Ptr<AST::Expr> node)
+{
+    if (node->desugarExpr) {
+        // Only check desugared nodes.
+        CheckLegalityOfReference(id, ctx, *node->desugarExpr);
+        return VisitAction::SKIP_CHILDREN;
+    } else if (auto ref = DynamicCast<NameReferenceExpr*>(node)) {
+        return CheckLegalityOfReferenceForNameReferenceExpr(ctx, ref);
     } else if (auto ae = DynamicCast<AssignExpr>(node)) {
         CheckMutationInStruct(ctx, *ae->leftValue);
     } else if (auto ide = DynamicCast<IncOrDecExpr>(node)) {

src/Sema/TypeCheckerImpl.h
@@ -19,6 +19,7 @@
 #include "ScopeManager.h"
 #include "TypeCheckUtil.h"
 #include "cangjie/AST/Clone.h"
+#include "cangjie/AST/Node.h"
 #include "cangjie/AST/Symbol.h"
 #include "cangjie/AST/Types.h"
 #include "cangjie/AST/Walker.h"
@@ -1600,6 +1601,7 @@ private:
     void CheckStaticMemberAccessLegality(const AST::MemberAccess& ma, const AST::Decl& target);
     void CheckInstanceMemberAccessLegality(const ASTContext& ctx, const AST::MemberAccess& ma, const AST::Decl& target);
     bool CheckLegalityOfReferenceIsSkip(Ptr<AST::Node> node);
+    AST::VisitAction CheckLegalityOfReferenceForNameReferenceExpr(ASTContext& ctx, Ptr<AST::NameReferenceExpr> nameRef);
     AST::VisitAction CheckLegalityOfReferenceForExpr(unsigned id, ASTContext& ctx, Ptr<AST::Expr> node);
     void CheckLegalityOfReference(unsigned id, ASTContext& ctx, AST::Node& node);
     void CheckLegalityOfReference(ASTContext& ctx, AST::Node& node);